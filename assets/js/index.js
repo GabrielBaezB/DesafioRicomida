25f2dad969c0f29c21902ccb96d315c130058573
GIBB <GIBB@V-sgibb.UAB.local>
2023-04-20 16:57:08 -0400

    Se agrega funciones de enviar correo en boton y de esconder texto en las tarjeta

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    $('#enviarCorreo').click(function() {
+        alert("El correo fue enviado correctamente...");
+    });
+    
+    $(".card-title").click(function() {
+        $(".card-text").toggle("slow", function() {
+  
+        });
+    });
+    
+  });
